0f7bc0d15f2dec1c34ec90b1927c2f3585a07e3a
Jorge Viana <secoviana@gmail.com>
2019-03-12 11:43:00 +0100

    removing duplication of test setup

--- src/test/java/StringCalculatorShould.java
+++ src/test/java/StringCalculatorShould.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.swing.*;
@@ -5,21 +6,26 @@ import javax.swing.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorShould {
+
+    private StringCalculator stringCalculator;
+
+    @BeforeEach
+    void setUp() {
+        stringCalculator = new StringCalculator();
+    }
+
     @Test
     void return_zero_for_empty_string() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
     void perform_add_when_input_is_a_single_number() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
     }
 
     @Test
     void returns_2_if_given_string_2() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(2, stringCalculator.add("2"));
     }
 }
