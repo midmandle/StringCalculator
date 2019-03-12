bdaa5da66c036810c26baf50d3f0d21dcb00e59c
Jorge Viana <secoviana@gmail.com>
2019-03-12 12:23:41 +0100

    refactoring adding two numbers

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,14 +1,18 @@
 public class StringCalculator {
     public int add(String numbers) {
-        if (numbers.length() == 1) {
+
+        if (numbers.isEmpty()) return 0;
+
+        String[] numbersAsStrings = numbers.split(",");
+
+        if (numbersAsStrings.length == 1) {
             int number = Integer.parseInt(numbers);
             return number;
         }
-        if(numbers.length() == 3) {
-            int number1 = Integer.parseInt(String.valueOf(numbers.charAt(0)));
-            int number2 = Integer.parseInt(String.valueOf(numbers.charAt(2)));
-            return number1 + number2;
-        }
-        return 0;
+
+        int number1 = Integer.parseInt(numbersAsStrings[0]);
+        int number2 = Integer.parseInt(numbersAsStrings[1]);
+
+        return number1 + number2;
     }
 }
--- src/test/java/StringCalculatorShould.java
+++ src/test/java/StringCalculatorShould.java
@@ -3,8 +3,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import javax.swing.*;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorShould {
